d1e0f46b45ad0004195a227a0f261348136b72d5
liweim <liweim@github.com>
2018-07-05 21:16:49 +0200

    improve roi

---
+++ cfsd18-perception-detectcone/src/cfsd18-perception-detectcone.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2018  Christian Berger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "detectcone.hpp"
+#include "collector.hpp"
+
+int32_t main(int32_t argc, char **argv) {
+    int32_t retCode{0};
+    std::map<std::string, std::string> commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+    if (commandlineArguments.count("cid")<1) {
+        std::cerr << argv[0] << " is a detectcone module for the CFSD18 project." << std::endl;
+        std::cerr << "Usage:   " << argv[0] << " --cid=<OpenDaVINCI session> [--id=<Identifier in case of simulated units>] [--verbose] [Module specific parameters....]" << std::endl;
+        std::cerr << "Example: " << argv[0] << " --cid=111 --id=120"  <<  std::endl;
+        retCode = 1;
+    } 
+    else {
+        const uint32_t WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
+        const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
+        const uint32_t BPP{static_cast<uint32_t>(std::stoi(commandlineArguments["bpp"]))};
+        bool offline{static_cast<bool>(std::stoi(commandlineArguments["offline"]))};
+        
+        uint32_t attentionSenderStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["attentionSenderStamp"])); 
+        uint32_t senderStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["senderStamp"]));
+        uint32_t stateMachineStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["stateMachineId"]));
+
+        // const bool VERBOSE{commandlineArguments.count("verbose") != 0};
+        bool sentReadySignal = false;
+        std::string filepathTimestamp = "/opt/timestamp/timestamps.txt";
+        
+        cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
+        DetectCone detectcone(commandlineArguments, od4);
+        int timeOutMs = std::stoi(commandlineArguments["timeDiffMilliseconds"]);
+        int separationTimeMs = std::stoi(commandlineArguments["separationTimeMs"]);
+        Collector collector(detectcone,timeOutMs,separationTimeMs,2);
+
+        cluon::data::Envelope data;
+        auto envelopeRecieved{[&logic = detectcone, senderStamp = attentionSenderStamp, &collector](cluon::data::Envelope &&envelope)
+            {
+                if(envelope.senderStamp() == senderStamp){
+                    collector.CollectCones(envelope);
+                }
+            } 
+        };
+
+        auto stateMachineStatusEnvelope{[&logic = detectcone, senderStamp = stateMachineStamp](cluon::data::Envelope &&envelope)
+        {
+            if(envelope.senderStamp() == senderStamp){
+                    logic.setStateMachineStatus(envelope);
+                }
+            }
+        };
+
+        od4.dataTrigger(opendlv::logic::perception::ObjectDirection::ID(),envelopeRecieved);
+        od4.dataTrigger(opendlv::logic::perception::ObjectDistance::ID(),envelopeRecieved);
+        od4.dataTrigger(opendlv::proxy::SwitchStateReading::ID(),stateMachineStatusEnvelope);
+
+        if(offline){
+            detectcone.getTimeStamp(filepathTimestamp);
+            while (od4.isRunning()) {
+                detectcone.checkLidarState();
+                cv::waitKey(1);
+            }
+        }
+        else{
+            if ( (BPP != 24) && (BPP != 8) ) {
+                std::cerr << argv[0] << ": bits per pixel must be either 24 or 8; found " << BPP << "." << std::endl;
+            }
+            else {
+                const uint32_t SIZE{WIDTH * HEIGHT * BPP/8};
+                const std::string NAME{(commandlineArguments["name"].size() != 0) ? commandlineArguments["name"] : "/camera1"};
+                const uint32_t ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+
+                (void)ID;
+                (void)SIZE;
+
+                std::ofstream file;
+                file.open(filepathTimestamp.c_str());
+                size_t frameCounter = 0;
+                std::string imgPath = "/opt/images/";
+                
+                std::unique_ptr<cluon::SharedMemory> sharedMemory(new cluon::SharedMemory{NAME});
+                if (sharedMemory && sharedMemory->valid()) {
+                    std::clog << argv[0] << ": Found shared memory '" << sharedMemory->name() << "' (" << sharedMemory->size() << " bytes)." << std::endl;
+
+                    cv::Size size;
+                    size.width = WIDTH;
+                    size.height = HEIGHT;
+
+                    IplImage *image = cvCreateImageHeader(size, IPL_DEPTH_8U, BPP/8);
+                    sharedMemory->lock();
+                    image->imageData = sharedMemory->data();
+                    image->imageDataOrigin = image->imageData;
+                    sharedMemory->unlock();
+                    bool readyState = false;
+                    bool runningState = false;
+                    while (od4.isRunning()) {
+                        // The shared memory uses a pthread broadcast to notify us; just sleep to get awaken up.
+                        sharedMemory->wait();
+                        
+                        sharedMemory->lock();
+                        image->imageData = sharedMemory->data();
+                        image->imageDataOrigin = image->imageData;
+                        cv::Mat img = cv::cvarrToMat(image); 
+
+                        cluon::data::TimeStamp imgTimestamp = cluon::time::now();
+                        sharedMemory->unlock();
+                        cv::waitKey(1);
+
+                        std::pair<cluon::data::TimeStamp, cv::Mat> imgAndTimeStamp(imgTimestamp, img);
+                        detectcone.getImgAndTimeStamp(imgAndTimeStamp);
+                        detectcone.checkLidarState();
+
+                        if(readyState){
+                            if(!sentReadySignal){
+                                std::cout << "detectcone module is ready!" << std::endl;
+                                sentReadySignal = true;
+                            }
+                            opendlv::system::SignalStatusMessage ssm;
+                            ssm.code(1);
+                            cluon::data::TimeStamp sampleTime = cluon::time::now();
+                            od4.send(ssm, sampleTime, senderStamp);
+                        }else{
+                            readyState = detectcone.getReadyState();
+                        }
+
+                        if(runningState){
+                            int64_t ts = cluon::time::toMicroseconds(imgTimestamp);
+                            file << std::setprecision(19) << ts << std::endl;
+                            std::string saveString = imgPath + std::to_string(frameCounter++) + ".png";
+                            cv::imwrite( saveString, img);
+                        }else{
+                            runningState = detectcone.getRunningState();
+                        }                       
+                    }
+                    file.close();
+                    cvReleaseImageHeader(&image);
+                }
+                else {
+                    std::cerr << argv[0] << ": Failed to access shared memory '" << NAME << "'." << std::endl;
+                }
+            }
+        
+        }
+    }
+    return retCode;
+}
+
+
+
+
+
+
+
+
+
---
+++ cfsd18-perception-detectcone/src/cone.cpp
@@ -0,0 +1,197 @@
+/**
+* Copyright (C) 2017 Chalmers Revere
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+* USA.
+*/
+
+#include "cone.hpp"
+
+
+Cone::Cone(double x,double y,double z):
+  m_pt()
+, m_prob()
+, m_label()
+, m_x(x)
+, m_y(y)
+, m_z(z)
+{
+}
+
+double Cone::getX(){
+  return m_x;
+}
+
+double Cone::getY(){
+  return m_y;
+}
+
+double Cone::getZ(){
+
+  return m_z;
+}
+
+size_t Cone::getLabel(){
+  return m_label;
+}
+
+void Cone::setX(double x){
+  //std::cout << "new x: " << x << " old x: " << m_x << std::endl;
+  m_x = x;
+}
+
+void Cone::setY(double y){
+  //std::cout << "new y: " << y << " old y: " << m_y << std::endl;
+  m_y = y;
+}
+void Cone::setZ(double z){
+  m_z = z;
+}
+void Cone::addHit(){
+  m_hits++;
+  m_missHit = 0;
+}
+int Cone::getHits(){
+  return m_hits;
+}
+void Cone::addMiss(){
+  m_missHit++;
+}
+
+int Cone::getMisses(){
+  return m_missHit;
+}
+
+bool Cone::isThisMe(double x, double y){
+  //double diffX = std::abs(m_x - x);
+  //double diffY = std::abs(m_y - y);
+  double distance = std::sqrt( (m_x - x)*(m_x - x) + (m_y - y)*(m_y - y) );
+  if(distance < 1.5){return true;}else{return false;}
+}
+
+bool Cone::checkColor(){
+  float totalDetected = (float)(m_bigOrangeCount+m_blueCount+m_smallOrangeCount+m_yellowCount);
+  int currentColorCount = 0;
+  if(totalDetected < 2.0f){
+    return false;
+  }
+  if((float)m_blueCount/totalDetected>0.70f){
+    m_label=1;
+    currentColorCount = m_blueCount;
+  }
+  else if((float)m_yellowCount/totalDetected>0.70f){
+    m_label=2;
+    currentColorCount = m_yellowCount;
+  }
+  else if((float)m_smallOrangeCount/totalDetected>0.70f){
+    m_label=3;
+    currentColorCount = m_smallOrangeCount;
+  }
+  else if((float)m_bigOrangeCount/totalDetected>0.70f){
+    m_label=4;
+    currentColorCount = m_bigOrangeCount;
+  }
+  if(currentColorCount>1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+  /*
+  int nBlue = 0;
+  int nYellow = 0;
+  int nSmallOrange = 0;
+  int nBigOrange = 0;
+  if(!(m_colorList.size()>2)){
+    return false;
+  }
+  for(uint32_t i = 1; i<5; i++){
+    for(uint32_t j = 0; j<m_colorList.size(); j++){
+      if(m_colorList[j]==i){
+        if(i == 1){
+          nBlue++;
+        }
+        else if(i == 2){
+          nYellow++;
+        }
+        else if(i == 3){
+          nSmallOrange++;
+        }
+        else if(i == 4){
+          nBigOrange++;
+        }
+      }
+    }
+  }
+  if(nBlue>=std::ceil(0.6*m_colorList.size())){
+    m_label = 1;
+    return true;
+  }
+  else if(nYellow>=std::ceil(0.6*m_colorList.size())){
+    m_label = 2;
+    return true;
+  }
+  else if(nSmallOrange>=std::ceil(0.6*m_colorList.size())){
+    m_label = 3;
+    return true;
+  }
+  else if(nBigOrange>=std::ceil(0.6*m_colorList.size())){
+    m_label = 4;
+    return true;
+  }
+  else{
+    return false;
+  }
+  */
+
+void Cone::addColor(size_t label){
+  if(label > 10 || label==0){
+    m_noDetectionCount++;
+  }
+  else if(label == 1){
+    m_blueCount++;
+  }
+  else if(label==2){
+    m_yellowCount++;
+  }
+  else if(label == 3){
+    m_smallOrangeCount++;
+  }
+  else if(label == 4){
+    m_bigOrangeCount++;
+  }
+}
+
+bool Cone::shouldBeInFrame(){
+  if(m_hits >= 2 && m_y > 0.2 && m_missHit < 2 && m_isValid && checkColor()){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+bool Cone::shouldBeRemoved(){
+  if(m_missHit >= 2 || m_y < 0.2 ){return true;}else{return false;}
+}
+
+void Cone::setValidState(bool state){
+  m_isValid = state;
+}
+
+bool Cone::isValid(){
+  return m_isValid;
+}
