bca49b29ea8607436315540476f86e22afd669f3
liweim <liweim@github.com>
2018-07-21 13:43:28 +0200

    include forwardDetection into backwardDetection

--- src/detectcone.hpp
+++ src/detectcone.hpp
@@ -140,6 +140,10 @@ class DetectCone {
   int m_orbPatchSize;
   std::string m_folderName;
   float m_maxZ;
+  std::ofstream m_file;
+  int m_blockSize;
+  int m_numDisp;
+  int m_uniRatio;
   
   const double DEG2RAD = 0.017453292522222; // PI/180.0
   const double RAD2DEG = 57.295779513082325; // 1.0 / DEG2RAD;
