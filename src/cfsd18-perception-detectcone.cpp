a5091b5c21d0a57cdbf3ad343b918235ac4b98a7
ruguangyou <512292346@qq.com>
2018-10-16 11:47:45 +0200

    Add some comments in main function

--- src/cfsd18-perception-detectcone.cpp
+++ src/cfsd18-perception-detectcone.cpp
@@ -26,19 +26,19 @@ int32_t main(int32_t argc, char **argv) {
         std::cerr << "Usage:   " << argv[0] << " --cid=<OpenDaVINCI session> [--id=<Identifier in case of simulated units>] [--verbose] [Module specific parameters....]" << std::endl;
         std::cerr << "Example: " << argv[0] << " --cid=111 --id=120"  <<  std::endl;
         retCode = 1;
-    } 
+    }
     else {
         const uint32_t WIDTH{static_cast<uint32_t>(std::stoi(commandlineArguments["width"]))};
         const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
         const uint32_t BPP{static_cast<uint32_t>(std::stoi(commandlineArguments["bpp"]))};
         bool offline{static_cast<bool>(std::stoi(commandlineArguments["offline"]))};
-        
-        uint32_t attentionSenderStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["attentionSenderStamp"])); 
+
+        uint32_t attentionSenderStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["attentionSenderStamp"]));
         uint32_t senderStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["senderStamp"]));
         uint32_t stateMachineStamp = static_cast<uint32_t>(std::stoi(commandlineArguments["stateMachineId"]));
 
         bool sentReadySignal = false;
-        
+
         cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
         DetectCone detectcone(commandlineArguments, od4);
         int timeOutMs = std::stoi(commandlineArguments["timeDiffMilliseconds"]);
@@ -46,25 +46,26 @@ int32_t main(int32_t argc, char **argv) {
         Collector collector(detectcone,timeOutMs,separationTimeMs,2);
 
         cluon::data::Envelope data;
-        auto envelopeRecieved{[&logic = detectcone, senderStamp = attentionSenderStamp, &collector](cluon::data::Envelope &&envelope)
-            {
-                if(envelope.senderStamp() == senderStamp){
-                    collector.CollectCones(envelope);
-                }
-            } 
-        };
 
-        auto stateMachineStatusEnvelope{[&logic = detectcone, senderStamp = stateMachineStamp](cluon::data::Envelope &&envelope)
-        {
-            if(envelope.senderStamp() == senderStamp){
-                    logic.setStateMachineStatus(envelope);
-                }
+        // lambda function, is to be called on newly arriving envelope in od4.dataTrigger()
+        auto envelopeRecieved{ [&logic = detectcone, senderStamp = attentionSenderStamp, &collector](cluon::data::Envelope &&envelope) {
+            // if the senderStamp of the newly arrived envelope equals to attentionSenderStamp
+            // then collect cones and store the information of current frame
+            if(envelope.senderStamp() == senderStamp)
+                collector.CollectCones(envelope);
             }
         };
 
-        od4.dataTrigger(opendlv::logic::perception::ObjectDirection::ID(),envelopeRecieved);
-        od4.dataTrigger(opendlv::logic::perception::ObjectDistance::ID(),envelopeRecieved);
-        od4.dataTrigger(opendlv::proxy::SwitchStateReading::ID(),stateMachineStatusEnvelope);
+        auto stateMachineStatusEnvelope{ [&logic = detectcone, senderStamp = stateMachineStamp](cluon::data::Envelope &&envelope) {
+            if(envelope.senderStamp() == senderStamp)
+                logic.setStateMachineStatus(envelope);
+            }
+        };
+
+        // on receiving Envelopes, OD4session can call a user-supplied lambda
+        od4.dataTrigger(opendlv::logic::perception::ObjectDirection::ID(), envelopeRecieved);
+        od4.dataTrigger(opendlv::logic::perception::ObjectDistance::ID(), envelopeRecieved);
+        od4.dataTrigger(opendlv::proxy::SwitchStateReading::ID(), stateMachineStatusEnvelope);
 
         std::stringstream currentDateTime;
         time_t ttNow = time(0);
@@ -95,19 +96,26 @@ int32_t main(int32_t argc, char **argv) {
 
         std::string command;
 
-        if(offline){
+        if(offline) {
             std::string fileName = "/opt/replay";
+            // infile is a ifstream object  to operate on the /opt/replay file
             std::ifstream infile(fileName);
-            if(infile.good()){
+            if(infile.good()) {
+                // if /opt/replay exists and is a file, remove it
                 command = "rm -r "+fileName;
                 system(command.c_str());
             }
+            // create a folder named /opt/replay
             command = "mkdir "+fileName;
             system(command.c_str());
+            // open a file named log.txt in the folder /opt/replay/
             detectcone.getFolderName(fileName);
+            // open the timestamps.txt file, and read it (this file should already exist)
             detectcone.getTimeStamp("/opt/timestamp/timestamps.txt");
+            // od4 keeps exchanging message, and checkLidarState() starts detection
             while (od4.isRunning()) {
                 detectcone.checkLidarState();
+                // waits 1 millisecond for showing a frame on an OpenCV window
                 cv::waitKey(1);
             }
         }
@@ -117,12 +125,14 @@ int32_t main(int32_t argc, char **argv) {
             }
             else {
                 const uint32_t SIZE{WIDTH * HEIGHT * BPP/8};
+                // the ["name"] should be the name of the shared memory area
                 const std::string NAME{(commandlineArguments["name"].size() != 0) ? commandlineArguments["name"] : "/camera1"};
                 const uint32_t ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
 
                 (void)ID;
                 (void)SIZE;
 
+                // folderName: yyyy-mm-dd-hh-mm-ss
                 command = "mkdir "+folderName;
                 system(command.c_str());
                 command = "mkdir "+folderName+"/timestamp/";
@@ -131,6 +141,7 @@ int32_t main(int32_t argc, char **argv) {
                 system(command.c_str());
                 command = "mkdir "+folderName+"/results/";
                 system(command.c_str());
+                // getFolderName() just opens log.txt file (maybe replace with a better name which would imply its functionality?)
                 detectcone.getFolderName(folderName+"/results");
 
                 std::string filepathTimestamp = folderName+"/timestamp/timestamps.txt";
@@ -138,33 +149,43 @@ int32_t main(int32_t argc, char **argv) {
                 std::ofstream file;
                 file.open(filepathTimestamp.c_str());
                 size_t frameCounter = 0, readyCounter = 0;
-                
+
                 std::unique_ptr<cluon::SharedMemory> sharedMemory(new cluon::SharedMemory{NAME});
                 if (sharedMemory && sharedMemory->valid()) {
                     std::clog << argv[0] << ": Found shared memory '" << sharedMemory->name() << "' (" << sharedMemory->size() << " bytes)." << std::endl;
 
+                    // an OpenCV class for specifying the size of an image
                     cv::Size size;
                     size.width = WIDTH;
                     size.height = HEIGHT;
 
+                    // creates an image header with (size, depth, channel), but does not allocate the image data
                     IplImage *image = cvCreateImageHeader(size, IPL_DEPTH_8U, BPP/8);
+                    // IplImage is taken from the Intel Image Processing Library,  OpenCV only supports a subset of possible IplImage formats
+
+                    // fetch image data from shared memory
                     sharedMemory->lock();
+                    // imageData is a pointer to aligned image data
                     image->imageData = sharedMemory->data();
+                    // imageDataOrigin is a pointer to very origin of image data (not necessarily aligned) - needed for correct deallocation
                     image->imageDataOrigin = image->imageData;
                     sharedMemory->unlock();
+
                     bool drivingState = false;
                     while (od4.isRunning()) {
                         // The shared memory uses a pthread broadcast to notify us; just sleep to get awaken up.
                         sharedMemory->wait();
-                        
+
+                        // ?? why need to assign the pointer over and over again?
                         sharedMemory->lock();
                         image->imageData = sharedMemory->data();
                         image->imageDataOrigin = image->imageData;
-                        cv::Mat img = cv::cvarrToMat(image); 
+                        // convert array (IplImage) to cv::Mat
+                        cv::Mat img = cv::cvarrToMat(image);
                         sharedMemory->unlock();
                         cv::waitKey(1);
 
-                        if(readyCounter++ > 150){
+                        if(readyCounter++ > 150) {
                             if(!sentReadySignal){
                                 std::cout << "detectcone module is ready!" << std::endl;
                                 sentReadySignal = true;
@@ -175,19 +196,25 @@ int32_t main(int32_t argc, char **argv) {
                             od4.send(ssm, sampleTime, senderStamp);
                         }
 
-                        if(drivingState){
+                        if(drivingState) {
                             cluon::data::TimeStamp imgTimestamp = cluon::time::now();
                             int64_t ts = cluon::time::toMicroseconds(imgTimestamp);
                             std::pair<int64_t, cv::Mat> imgAndTimeStamp(ts, img);
+                            // set timestamp and the corresponding image (cv::Mat)
                             detectcone.setTimeStamp(imgAndTimeStamp);
                             detectcone.checkLidarState();
+                            // write timestamp in microsecond to timestamps.txt
                             file << std::setprecision(19) << ts << std::endl;
+                            // save frame image in the opt/images/ folder, the image is named by frameCounter
                             std::string saveString = imgPath + std::to_string(frameCounter++) + ".png";
+                            // std::thread threadName (classFunction, class, arguments..)
                             std::thread imWriteThread(&DetectCone::saveImages,&detectcone,saveString,img);
+                            // detache the thread from the calling thread, allowing them to execute independently from each other
                             imWriteThread.detach();
-                        }else{
+                        }
+                        else {
                             drivingState = detectcone.getdrivingState();
-                        }                       
+                        }
                     }
                     file.close();
                     cvReleaseImageHeader(&image);
@@ -200,16 +227,8 @@ int32_t main(int32_t argc, char **argv) {
                     od4.send(ssm, sampleTime, senderStamp);
                 }
             }
-        
+
         }
     }
     return retCode;
 }
-
-
-
-
-
-
-
-
